f9b771900d8c8ff9012fb27c673a54fe39c2bfaf
s.stetsenko <s.stetsenko@ceteralabs.com>
2020-06-13 15:23:02 +0300

    Правки связанные с английской версией сайта

--- home/telecom-sb/telecomsb.ru/docs/about/index.php
+++ home/telecom-sb/telecomsb.ru/docs/about/index.php
@@ -9,7 +9,6 @@ $res = CIBlockElement::GetList(Array("NAME"=>"ASC"), $arFilter,false,false, $arS
 while($ob = $res->GetNextElement()){
     $arFields = $ob->GetFields();
 }?>
-
 <div class="page-content-wrapper">
     <div class="page-content about-page">
         <div class="top-bg-section padded">
